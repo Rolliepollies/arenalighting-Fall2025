ebfc05bae7bf4e2d343b7cfb051513fbca3ec04c
nmkliesner4 <nmk256@gmail.com>
2025-10-07 13:32:55 -0500

    cleaned up the way LEDs are created and general cleanup

--- ShowBuilder/ShowBuilder.py
+++ ShowBuilder/ShowBuilder.py
@@ -6,49 +6,24 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QColor, QBrush, QPainter
-
-ledLocations = []
-
-def grabPositions():
-    index = 0
-    with open("showBuilder/postions.txt", 'r', encoding='utf-8') as f:
-        for line in f:
-            line = line.split(',')
-            x = float(line[0].strip())
             
-            z = float(line[1].strip())#we will refer to this as y now on since it makes more sense in a 2d space
-            
-            ledLocations.append([x * 30, z * 30, index])
-            index += 1
-            
-
-grabPositions()
-
-
-def set_selected_color(self):
-    color = QColorDialog.getColor()
-    if not color.isValid():  
-        return
-    for led in self.leds:
-        if led.isSelected():
-            led.set_color(color)
 
 class LEDItem(QGraphicsEllipseItem):
     def __init__(self, x, y, index, size=10):
         super().__init__(-size/2, -size/2, size, size)
         self.setPos(x, y)
         self.index = index
-        self.setBrush(QBrush(Qt.gray))
-        self.color = QColor(Qt.gray)
+        self.brush = QBrush(Qt.gray)
+        self.setBrush(self.brush)
         self.setFlag(QGraphicsEllipseItem.ItemIsSelectable, True)
 
     def set_color(self, color: QColor):
-        self.color = color
-        self.setBrush(QBrush(color))
+        self.brush.setColor(color)
+        self.setBrush(self.brush)
 
 
 class LEDViewer(QMainWindow):
-    def __init__(self, led_positions):
+    def __init__(self):
         super().__init__()
         self.setWindowTitle("LED Mapper")
         self.resize(800, 600)
@@ -60,11 +35,7 @@ class LEDViewer(QMainWindow):
         self.view.setDragMode(QGraphicsView.RubberBandDrag)  # allow box selection
 
         # Create LEDs
-        self.leds = []
-        for (x, y, index) in led_positions:
-            led = LEDItem(x, y, index)
-            self.scene.addItem(led)
-            self.leds.append(led)
+        self.create_LEDs()
 
         # Buttons
         self.save_button = QPushButton("Save Frame")
@@ -86,7 +57,7 @@ class LEDViewer(QMainWindow):
         # Timer for animation (placeholder)
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_effect)
-        self.frame = 0
+        self.frameNumber = 0
         self.timer.start(100)  # 10 fps
         self.hue_slider = QSlider(Qt.Horizontal)
         self.hue_slider.setRange(0, 359)
@@ -95,6 +66,22 @@ class LEDViewer(QMainWindow):
         self.hue_slider.valueChanged.connect(lambda v: self.hue_label.setText(f"Hue: {v}"))
         layout.addWidget(self.hue_label)
         layout.addWidget(self.hue_slider)
+
+    def create_LEDs(self):
+        self.leds = []
+        index = 0
+        with open("showBuilder/postions.txt", 'r', encoding='utf-8') as f:
+            for line in f:
+                line = line.split(',')
+                
+                x = float(line[0].strip())
+                z = float(line[1].strip()) # We will refer to this as y now on since it makes more sense in a 2d space
+                
+                led = LEDItem(x*40, z*40, index)
+                self.scene.addItem(led)
+                self.leds.append(led)
+
+                index += 1
 
     def update_effect(self):
         # No automatic animation for now
@@ -106,9 +93,6 @@ class LEDViewer(QMainWindow):
         for led in self.leds:
             if led.isSelected():
                 led.set_color(color)
-                
-                
-    
 
     def save_frame(self):
         print('hello')
@@ -116,7 +100,7 @@ class LEDViewer(QMainWindow):
         color_map = {}
         group_id = 1
         for led in self.leds:
-            r, g, b, a = led.color.getRgbF()
+            r, g, b, a = led.brush.color().getRgbF()
             rgb = (r, g, b, a)
             if rgb not in color_map:
                 # create new group entry
@@ -143,18 +127,13 @@ class LEDViewer(QMainWindow):
         frame_data = {
             "groups": groups
         }
+
         print('end')
-        print(str(self.frame) )
-        filename = f"frame_{self.frame}.json"
+        print(str(self.frameNumber) )
+        folder = "ShowBuilderTest"
+        filename = f"./stadium/Assets/Resources/{folder}/frame_{self.frameNumber}.json"
         with open(filename, "w", encoding="utf-8") as f:
             json.dump(frame_data, f, indent=2)
-
-
-
-
-        
-        
-        
         
     # Zoom with mouse wheel
     def wheelEvent(self, event):
@@ -168,6 +147,6 @@ class LEDViewer(QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    viewer = LEDViewer(ledLocations)
+    viewer = LEDViewer()
     viewer.show()
     sys.exit(app.exec_())
